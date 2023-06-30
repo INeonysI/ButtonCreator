183c3a8349b68d5db45fb59c24d4e746d950009f
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-06-30 08:26:15 -0300

    Mini projeto onde o usuário pode personalizar um botão através de um menu. Após finalizar a personalização, o usuário pode copiar o código css do botão.

--- script.js
+++ script.js
@@ -1,34 +1,6 @@
-const rangeInputs = document.querySelectorAll('input[type="range"]');
-
-function handleInputChange(e) {
-  let target = e.target;
-  if (e.target.type !== "range") {
-    target = document.getElementById("range");
-  }
-  const min = target.min;
-  const max = target.max;
-  const val = target.value;
-
-  target.style.backgroundSize = ((val - min) * 100) / (max - min) + "% 100%";
-}
-
-rangeInputs.forEach((input) => {
-  input.addEventListener("input", handleInputChange);
-});
-
 const form = document.querySelector("#buttonCreator");
 const btn = document.querySelector(".btn");
-
-form.addEventListener("change", handleChange);
-
-function handleChange(event) {
-  const name = event.target.name;
-  const value = event.target.value;
-
-  handleStyle[name](value);
-  showCss();
-}
-
+const rangeInputs = document.querySelectorAll('input[type="range"]');
 const handleStyle = {
   element: btn,
   text(value) {
@@ -60,6 +32,42 @@ const handleStyle = {
   },
 };
 
+//Adiciona o comportamento do background dos inputs do tipo range
+rangeInputs.forEach((input) => {
+  input.addEventListener("input", handleInputChange);
+  input.style.backgroundSize =
+    ((localStorage[input.id] - input.min) * 100) / (input.max - input.min) +
+    "% 100%";
+});
+//Adiciona o comportamento de modificar o estilo do botão através do menu e exibir o código
+form.addEventListener("change", handleChange);
+//Adiciona o comportamento de copiar o código através do ícone
+const copy = document.querySelector(".copy");
+copy.addEventListener("click", cpyToClipboard);
+//Atualiza os valores com base no conteúdo do localStorage
+setValues();
+
+function handleChange(event) {
+  const name = event.target.name;
+  const value = event.target.value;
+
+  handleStyle[name](value);
+  saveValue(name, value);
+  showCss();
+}
+
+function handleInputChange(e) {
+  let target = e.target;
+  if (e.target.type !== "range") {
+    target = document.getElementById("range");
+  }
+  const min = target.min;
+  const max = target.max;
+  const val = target.value;
+
+  target.style.backgroundSize = ((val - min) * 100) / (max - min) + "% 100%";
+}
+
 function showCss() {
   const css = document.querySelector(".code");
 
@@ -70,11 +78,23 @@ function showCss() {
   css.innerHTML = text;
 }
 
-const copy = document.querySelector(".copy");
-copy.addEventListener("click", cpyToClipboard);
-
 function cpyToClipboard() {
   const value = `.btn { \n\ ${document.querySelector(".code").innerText} \n\}`;
   console.log(value);
   navigator.clipboard.writeText(value);
+}
+
+function saveValue(name, value) {
+  localStorage[name] = value;
+}
+
+function setValues() {
+  const properties = Object.keys(localStorage);
+  if (properties) {
+    properties.forEach((propertie) => {
+      handleStyle[propertie](localStorage[propertie]);
+      form.elements[propertie].value = localStorage[propertie];
+      showCss;
+    });
+  }
 }
